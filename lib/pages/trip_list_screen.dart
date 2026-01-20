c1fcaed2673ac05659fc4009428ec19e024a91bb
dhruv282 <dhruvpatel282.dp@gmail.com>
2026-01-19 19:17:46 -0500

    Added circular loader

--- lib/pages/trip_list_screen.dart
+++ lib/pages/trip_list_screen.dart
@@ -34,63 +34,67 @@ class _TripListScreenState extends State<TripListScreen> {
   @override
   Widget build(BuildContext context) {
     final tripsProvider = Provider.of<TripsProvider>(context);
-    return Scaffold(
-      appBar: AppBar(title: const Text('Saved Trips')),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: const Icon(Icons.eco),
-        label: const Text('Start Trip'),
-        onPressed: () async {
-          await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => const DriveScreen()),
-          );
-        },
-      ),
-      body: ListView.builder(
-        itemCount: tripsProvider.trips.length,
-        itemBuilder: (c, i) {
-          final t = tripsProvider.trips[i];
-          return Card(
-            child: ListTile(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              title: Text('Trip ${t.start}'),
-              subtitle: Text('${t.samples.length} samples'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => DriveScreen(viewTrip: t)),
-                );
-              },
-              onLongPress:
-                  () => showDialog(
-                    context: context,
-                    builder:
-                        (c) => AlertDialog(
-                          title: const Text('Delete Trip?'),
-                          content: const Text(
-                            'Are you sure you want to delete this trip?',
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.pop(c),
-                              child: const Text('Cancel'),
-                            ),
-                            TextButton(
-                              onPressed: () async {
-                                await tripsProvider.deleteTrip(t);
-                                if (c.mounted) Navigator.pop(c);
-                              },
-                              child: const Text('Delete'),
-                            ),
-                          ],
-                        ),
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Scaffold(
+          appBar: AppBar(title: const Text('Saved Trips')),
+          floatingActionButton: FloatingActionButton.extended(
+            icon: const Icon(Icons.eco),
+            label: const Text('Start Trip'),
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const DriveScreen()),
+              );
+            },
+          ),
+          body: ListView.builder(
+            itemCount: tripsProvider.trips.length,
+            itemBuilder: (c, i) {
+              final t = tripsProvider.trips[i];
+              return Card(
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-            ),
-          );
-        },
-      ),
-    );
+                  title: Text('Trip ${t.start}'),
+                  subtitle: Text('${t.samples.length} samples'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => DriveScreen(viewTrip: t),
+                      ),
+                    );
+                  },
+                  onLongPress:
+                      () => showDialog(
+                        context: context,
+                        builder:
+                            (c) => AlertDialog(
+                              title: const Text('Delete Trip?'),
+                              content: const Text(
+                                'Are you sure you want to delete this trip?',
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.pop(c),
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    await tripsProvider.deleteTrip(t);
+                                    if (c.mounted) Navigator.pop(c);
+                                  },
+                                  child: const Text('Delete'),
+                                ),
+                              ],
+                            ),
+                      ),
+                ),
+              );
+            },
+          ),
+        );
   }
 }
